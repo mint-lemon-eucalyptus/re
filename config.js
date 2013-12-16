309e49412849c0875f87e9759379ce97c065fb10
mint-lemon-eucalyptus <mint.lemon.eucalyptus@gmail.com>
2013-12-16 20:09:03 +0400

    up

---
+++ config.js
@@ -0,0 +1,7 @@
+//each excercise must have interval of allowed index numbers of nodes and relations
+module.exports=
+    {
+        defaultExcerciseTextAreaContent:'START root=node(0) // Start with the reference node\n'+
+        'RETURN root        // and return it.'
+    }
+;
