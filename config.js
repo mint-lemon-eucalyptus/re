41c0915057527f1e2f91dfc3072ce2f43ebd3e49
mint-lemon-eucalyptus <mint.lemon.eucalyptus@gmail.com>
2013-12-19 22:45:59 +0400

    up

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 module.exports=
     {
         defaultExcerciseTextAreaContent:
-          'MATCH (n:myLabel) RETURN n LIMIT 3'//  'START root=node(0) // Start with the reference node\n'+        'RETURN root        // and return it.'
+            'MATCH (a)-[r]-(b) RETURN a,b,r LIMIT 25',
+//          'MATCH (n:myLabel) \nRETURN n LIMIT 3',//  'START root=node(0) // Start with the reference node\n'+        'RETURN root        // and return it.'
+        testLabelsPrafix:'testL'
     }
 ;
